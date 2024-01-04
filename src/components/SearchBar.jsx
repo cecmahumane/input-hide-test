a40506bad54462e42b770aab959cbd74b0663719
cecmahumane <cecmahumane@gmail.com>
2024-01-04 13:07:55 -0500

    Input dropdown built. Still needs to be debugged.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './SearchBar.css'
+import { useState } from "react";
 
-const SearchBar = () => {
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
   return (
-    <div className='input-wrapper'>
-        <input placeholder='type to search...'/>
+    <div className="input-wrapper">
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
-  )
-}
-
-export default SearchBar
+  );
+};
