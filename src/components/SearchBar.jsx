e63c58177d9aa0a8343597f2b06ce2d9dce2af89
cecmahumane <cecmahumane@gmail.com>
2024-01-04 08:21:09 -0500

    first commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchBar.css'
+
+const SearchBar = () => {
+  return (
+    <div className='input-wrapper'>
+        <input placeholder='type to search...'/>
+    </div>
+  )
+}
+
+export default SearchBar
